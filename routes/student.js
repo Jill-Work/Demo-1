707fa377c673c10323b9f76f0861826ec75a4aec
Jill-7span <113359267+Jill-7span@users.noreply.github.com>
2022-10-05 17:04:43 +0530

    direct auth

--- routes/student.js
+++ routes/student.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const studentMiddelware = require("../middleware/studentMiddleware")
+const studentMiddleware = require("../middleware/studentMiddleware")
 
 
 const studentController = require('../controller/studentController');
 
 
-router.get('/me',studentMiddelware.studentAuth)
+router.get('/me',studentMiddleware.studentAuth)
 
 router.get('/:id',studentController.getStudent);
 
 router.get('/',studentController.getStudents);
 
-router.post('/',studentMiddelware.insertStudent,studentController.insertStudent);
+router.post('/',studentMiddleware.insertStudent,studentController.insertStudent);
 
-router.put('/:id',studentMiddelware.updateStudent,studentController.updateStudent);
+router.put('/:id',studentMiddleware.updateStudent,studentController.updateStudent);
 
 router.delete('/:id',studentController.deleteStudent);
 
